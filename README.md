695994cedaeb6a435395afc4d08bd9bb9ceebd7b
Goopy25 <raylee25@proton.me>
2024-07-21 07:25:29 -0800

    Updated the README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-This is a new copy of code I started at work
+# ASCII World Map Maker
+
+Makes a colored ASCII random fantasy world map
+
+## Functionality
+
+Currently the program can generate a table of colored ASCII characters with a specified width, height, and generation method. Generation methods include **perlin noise** (of which the scale and seed can be inputted), a black-to-white **gradient**, black-to-white horizontal and vertical **reflected gradients**, and **monocolored** (the color can be specified).
+
+Additionally, maps can be filled with the blended output of two other maps. Current blend modes are **"multiply"** and **"screen."**
+
+The maps can be displayed in the console with **.display()** or **.log()**. The former will create the colored ASCII art, and can optionally display a second map next to it to make visual comparisons easier. The latter will log the values of the map, row by row. This is useful for debugging.
+
+## Tricks
+
+- By recursively using the screen blend mode, contrast can be added to the map. This is made easier with the **Contrast** function.
+
+- By multiplying a horizontal and vertical reflected gradient together, a **vignette** map can be made. The vignette can be constricted or widened by multiplying it or screening it against itself.
+
+## Planned Features
+
+- Create humidity & temperature maps, determine climate based off the compared values
+
+- Implement *rain shadow* by using an emboss filter (or something similar) to affect the humidity map
+
+- Function to insert a specified symbol at any (x, y) coordinate
+
+***
+
+*This is a project I started at work, but now has become a personal project. I don't quite get how Git or GitHub work* totally *yet, so that's why I didn't create a fork of my original project, and just downloaded the .ZIP of the old code and started over.*
---
+++ maps.js
@@ -0,0 +1,159 @@
+import chalk from "chalk";
+import { ValueNoise } from "value-noise-js";
+
+export class Map {
+    constructor(name = "Map", width, height, zoom = 1, seed = "") {
+        this.name = name;
+        this.map = [];
+        this.noise = new ValueNoise(seed);
+        this.width = width;
+        this.height = height;
+        this.zoom = zoom;
+    }
+    fill(blendMode, layer0, layer1, fillPercent, ) {
+        switch(blendMode) {
+            case "normal":
+                this.blendMode = "normal";
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    for (let i = 0; i < this.width; i++) {
+                        row.push(layer0.map[j][i]);
+                    }
+                    this.map.push(row);
+                }
+                break;
+            case "multiply":
+                this.blendMode = "multiply";
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    for (let i = 0; i < this.width; i++) {
+                        row.push( Math.floor(layer1.map[j][i] * layer0.map[j][i] * 0.01) );
+                    }
+                    this.map.push(row);
+                }
+                break;
+            case "screen":
+                this.blendMode = "screen";
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    for (let i = 0; i < this.width; i++) {
+                        let a = layer0.map[j][i] * 0.01;
+                        let b = layer1.map[j][i] * 0.01;
+                        let value = 1 - ( ( 1 - a ) * ( 1 - b ) );
+                        row.push(Math.floor(value * 100));
+                    }
+                    this.map.push(row);
+                }
+                break;
+            case "gradient":
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    for (let i = 0; i < this.width; i++) {
+                        row.push((100 / this.height) * j);
+                    }
+                    this.map.push(row);
+                }
+                break;
+            case "reflected":
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    if (j < this.height / 2) {
+                        for (let i = 0; i < this.width; i++) {
+                            row.push((100 / Math.floor(this.height / 2)) * j);
+                        }
+                    } else {
+                        for (let i = 0; i < this.width; i++) {
+                            row.push(this.map[Math.abs(j - this.height)-1][i]);
+                        }
+                    }
+                    this.map.push(row);
+                }
+                break;
+            case "reflected2":
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    for (let i = 0; i < this.width; i++) {
+                        if (i < this.width / 2) {
+                            row.push((100 / Math.floor(this.width / 2)) * i);
+                        } else {
+                            row.push(row[Math.abs(i - this.width) - 1]);
+                            // row.push("!");
+                        }
+                    }
+                    this.map.push(row);
+                }
+                break;
+            case "fill":
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    for (let i = 0; i < this.width; i++) {
+                        row.push(fillPercent);
+                    }
+                    this.map.push(row);
+                }
+                break;
+            default:
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    for (let i = 0; i < this.width; i++) {
+                        row.push(Math.floor(this.noise.evalXY(i * this.zoom, j * this.zoom) * 100));
+                    }
+                    this.map.push(row);
+                }
+        }
+    }
+    log() {
+        console.log("\n" + this.name + " Log");
+        for (let j = 0; j < this.map.length; j++) {
+            console.log(`Row (${j}): ` + this.map[j].join(", "));
+        }
+    }
+    display(compareBool = false, compareTo) {
+        let displayMapA = [];
+        let displayMapB = [];
+        switch(compareBool) {
+            case true:
+                for (let j = 0; j < this.height; j++) {
+                    let rowA = [];
+                    let rowB = [];
+                    for (let i = 0; i < this.width; i++) {
+                        let x = Math.floor(this.map[j][i] * 2.55);
+                        rowA.push(chalk.bgRgb(x, x, x)(" "));
+
+                        let y = Math.floor(compareTo.map[j][i] * 2.55);
+                        rowB.push(chalk.bgRgb(y, y, y)(" "));
+                    }
+                    displayMapA.push(rowA);
+                    displayMapB.push(rowB);
+                }
+                console.log(this.name + " vs. " + compareTo.name);
+                for (let j = 0; j < displayMapA.length; j++) {
+                    console.log((displayMapA[j].join("")) + " " + (displayMapB[j].join("")));
+                }
+                break;
+            default:
+                for (let j = 0; j < this.height; j++) {
+                    let row = [];
+                    for (let i = 0; i < this.width; i++) {
+                        let x = Math.floor(this.map[j][i] * 2.55);
+                        row.push(chalk.bgRgb(x, x, x)(" "));
+                    }
+                    displayMapA.push(row);
+                }
+                console.log(this.name);
+                for (let j = 0; j < displayMapA.length; j++) {
+                    console.log(displayMapA[j].join(""));
+                }
+        }
+    }
+}
+
+export function Contrast(map) {
+    let temp = new Map("", map.width, map.height);
+    temp.fill("screen", map, map);
+    return temp;
+}
+
+export function Insert(symbol, map, x, y) {
+    
+}
